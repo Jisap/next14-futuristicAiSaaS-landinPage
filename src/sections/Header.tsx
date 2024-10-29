90291d1fa39a18a4fe52d2b0baea29b034937388
Jorge Matos <jisapjisap@gmail.com>
2024-10-29 18:31:14 +0100

    button header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -33,7 +33,7 @@ export const Header = () => {
   return (
     <header className="border-b border-gray-200/20">
       <div className="container">
-        <div className="h-18 flex items-center">
+        <div className="h-18 flex justify-between items-center">
           <div className="flex gap-4 items-center">
             <div 
               className=" size-10 bg-gray-200 bg-[conic-gradient(from_45deg,_var(--color-violet-400),_var(--color-fuchsia-400),_var(--color-amber-300),_var(--color-teal-300),_var(--color-violet-400))]"
@@ -46,9 +46,13 @@ export const Header = () => {
           </div>
           <div></div>
           <div>
-            <button>
-              <div></div>
-              <div></div>
+            <button className="size-10 rounded-lg border-2 relative">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-4 h-0.5 bg-gray-100 -translate-y-1"></div>
+              </div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/">
+                <div className="w-4 h-0.5 bg-gray-100 translate-y-1"></div>
+              </div>
             </button>
           </div>
         </div>
