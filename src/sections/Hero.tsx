e543eff733b6fc3782661fbf4ca1faac576d1348
Jorge Matos <jisapjisap@gmail.com>
2024-11-02 17:52:17 +0100

    mask-image

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/Button';
 import Image from 'next/image';
 import underlineImage from '@/assets/images/underline.svg?url'
 import Loader from '@/assets/images/loader.svg'
+import { Orbit } from '@/components/Orbit';
 
 
 export const Hero = () => {
@@ -12,17 +13,34 @@ export const Hero = () => {
     <section className="pb-[1000px]">
       <div className="container">  
         <div className='border-l border-r border-[var(--color-border)]'>
-          <div className='container py-24 relative isolate'>
+          {/* mask-image para difuminar el comienzo superior e inferior del div */}
+          <div className='container py-24 relative isolate overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]'>
             {/* Aquí se especifica un gradiente radial que comienza desde un círculo que se extiende hasta la esquina más lejana del elemento (farthest-corner) 
             Esto genera un fondo radial que pasa de fuchsia a indigo y termina en transparencia. */}
             {/* mask-image crea un círculo que se extiende hasta el lado más lejano del contenedor, 
             black:  el área en color negro será opaca (visible), mostrando el contenido del gradiente de fondo.
             transparent: esta zona será transparente, haciendo que todo lo que esté en esa parte del círculo se vuelva invisible.*/}
             <div className='
-              absolute inset-0 -z-10
+              absolute inset-0 -z-10 
               bg-[radial-gradient(circle_farthest-corner,_var(--color-fuchsia-900)_50%,_var(--color-indigo-900)_75%,transparent)]
               [mask-image:radial-gradient(circle_farthest-side,black,transparent)]'
-            >  
+            ></div>
+            <div className='absolute inset-0 -z-10'>
+              <div className='absolute-center'>
+                <Orbit className='size-[350px]' />
+              </div>
+              <div className='absolute-center'>
+                <Orbit className='size-[600px]' />
+              </div>
+              <div className='absolute-center'>
+                <Orbit className='size-[850px]' />
+              </div>
+              <div className='absolute-center'>
+                <Orbit className='size-[1100px]' />
+              </div>
+              <div className='absolute-center'>
+                <Orbit className='size-[1350px]' />
+              </div>
             </div>
             <h1 className='text-4xl font-semibold text-gray-100 text-center leading-tight'>
               Unlock the Future of AI Conversations with{" "}
