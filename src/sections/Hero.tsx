c9897cf1c2f976600c11c8e7a7a2c10f703ca175
Jorge Matos <jisapjisap@gmail.com>
2024-11-02 18:43:56 +0100

    planet

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -74,16 +74,25 @@ export const Hero = () => {
               </Button>
             </div>
 
-            <div className='relative mt-20 rounded-2xl border-2 overflow-hidden border-gradient flex justify-center'>
-              <Image 
-                src={robotImg}
-                alt="robot"
-              />
-              <div className='absolute bottom-2 md:bottom-4 left-1/2 -translate-x-1/2 w-full px-4 flex justify-center'>
-                <div className='bg-gray-950/80 flex items-center justify-center gap-4 px-4 py-2 rounded-2xl w-[320px] max-w-full'>
-                  <Loader className='text-violet-400'/>
-                  <div className='font-semibold text-xl text-gray-100'>
-                    AI is generating<span>|</span>
+            <div className='relative'>
+              {/* planets */}
+              <div className='absolute left-1/2 top-0'>
+                <div className='bg-gradient-to-b from-violet-400 to-gray-950 size-8 rounded-full
+                -translate-x-[316px] -translate-y-[77px] rotate-135'></div>
+              </div>
+              <div className='relative mt-20 rounded-2xl border-2 overflow-hidden border-gradient flex'>
+                {/* image robot */}
+                <Image 
+                  src={robotImg}
+                  alt="robot"
+                />
+                {/* input */}
+                <div className='absolute bottom-2 md:bottom-4 left-1/2 -translate-x-1/2 w-full max-w-xs px-[15px] flex justify-center'>
+                  <div className='bg-gray-950/80 flex items-center justify-center gap-4 px-4 py-2 rounded-2xl w-[320px] max-w-full'>
+                    <Loader className='text-violet-400'/>
+                    <div className='font-semibold text-xl text-gray-100'>
+                      AI is generating<span>|</span>
+                    </div>
                   </div>
                 </div>
               </div>
