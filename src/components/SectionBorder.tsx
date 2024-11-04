a9adf9cfdab31982d1965e2a16889a0f5eb00ed7
Jorge Matos <jisapjisap@gmail.com>
2024-11-04 15:26:22 +0100

    section border

--- src/components/SectionBorder.tsx
+++ src/components/SectionBorder.tsx
@@ -5,13 +5,16 @@ import { twMerge } from "tailwind-merge"
 
 
 export const SectionBorder = (props: HTMLAttributes<HTMLDivElement>) => {
+
+  const { className, ...otherProps} = props
+
   return (
     <div className={
       twMerge(
         'border-l border-r border-[var(--color-border)]', 
-        props.className
+        className
       )}
-      {...props}
+      {...otherProps}
     />
   )
 }
