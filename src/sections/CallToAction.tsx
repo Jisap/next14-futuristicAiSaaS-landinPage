a3bcf6ae0d81b39c55721ec8a7827cfe4296fb5b
Jorge Matos <jisapjisap@gmail.com>
2024-11-08 20:14:46 +0100

    callToAction

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,18 +1,22 @@
 import { SectionBorder } from "@/components/SectionBorder";
 import { SectionContent } from "./SectionContent";
+import { Button } from "@/components/Button";
 
 
 export const CallToAction = () => {
   return (
     <section className="pb-[1000px]">
       <div className="container">
-        <SectionBorder>
+        <SectionBorder borderTop>
           <SectionContent>
-            <h2>Join the AI Revolution wiht Sphereal</h2>
-            <p>
+            <h2 className="text-gray-200 font-semibold text-3xl text-center leading-tight">
+              Join the AI Revolution wiht <span>Sphereal</span>
+            </h2>
+            <p className="text-center text-xl mt-8">
               Experience the transformative power of Ai with Sphereal. Boost your productivity and
               streamline your workflow with our innovative AI-chat platform.
             </p>
+            <Button>Get Started</Button>
           </SectionContent>
         </SectionBorder>
       </div>
