191198d4ca2cefeca990cdc17cac11c4756ca6f7
Jorge Matos <jisapjisap@gmail.com>
2024-11-08 20:12:07 +0100

    callToAction

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,5 +1,23 @@
+import { SectionBorder } from "@/components/SectionBorder";
+import { SectionContent } from "./SectionContent";
+
+
 export const CallToAction = () => {
-  return <section>CallToAction</section>;
+  return (
+    <section className="pb-[1000px]">
+      <div className="container">
+        <SectionBorder>
+          <SectionContent>
+            <h2>Join the AI Revolution wiht Sphereal</h2>
+            <p>
+              Experience the transformative power of Ai with Sphereal. Boost your productivity and
+              streamline your workflow with our innovative AI-chat platform.
+            </p>
+          </SectionContent>
+        </SectionBorder>
+      </div>
+    </section>
+  )
 };
 
 export default CallToAction;
