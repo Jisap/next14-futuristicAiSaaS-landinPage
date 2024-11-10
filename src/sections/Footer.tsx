4c105d1bb41995eb35b62040bf3067fdb1207ed5
Jorge Matos <jisapjisap@gmail.com>
2024-11-10 16:50:06 +0100

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -49,9 +49,9 @@ export const Footer = () => {
   return (
     <footer className="border-t border-[var(--color-border)]">
       <div className="container py-8">
-        <div className="flex flex-col items-center gap-8">
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center gap-8">
           <div className="font-extrabold text-2xl">sphereal.ai</div>
-          <nav className="flex flex-col gap-8 items-center">
+          <nav className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             {navItems.map((item) => (
               <a href={item.href} key={item.href} className="uppercase text-xs tracking-widest font-bold text-gray-400">
                 {item.name}
@@ -59,7 +59,7 @@ export const Footer = () => {
             ))}
           </nav>
         </div>
-        <div className="mt-16 flex flex-col items-center gap-8">
+        <div className="mt-16 flex flex-col md:flex-row-reverse md:justify-between items-center gap-8">
           <div className="flex justify-center gap-6">
             {socialLinks.map((link) => (
               <a href={link.href} key={link.name}>
