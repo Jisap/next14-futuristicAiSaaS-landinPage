e380327fb8ab22ea69682624ad0f2b8abf2fa600
Jorge Matos <jisapjisap@gmail.com>
2024-11-12 18:05:54 +0100

    hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import robotImg from '@/assets/images/robot.jpg'
 import { Button } from '@/components/Button';
 import Image from 'next/image';
 import underlineImage from '@/assets/images/underline.svg?url'
-import Loader from '@/assets/images/loader.svg'
+import Loader from '@/assets/images/loader-animated.svg'
 import { Orbit } from '@/components/Orbit';
 import { Planet } from '@/components/Planet';
 import { SectionBorder } from '@/components/SectionBorder';
@@ -126,7 +126,7 @@ export const Hero = () => {
                     <div className='bg-gray-950/80 flex items-center justify-center gap-4 px-4 py-2 rounded-2xl w-[320px] max-w-full'>
                       <Loader className='text-violet-400'/>
                       <div className='font-semibold text-xl text-gray-100'>
-                        AI is generating<span>|</span>
+                        AI is generating<span className='animate-cursor-blink'>|</span>
                       </div>
                     </div>
                   </div>
