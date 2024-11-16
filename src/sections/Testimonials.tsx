a280ed19a17fcb1dd078ade8005c8f15d0cfb98c
Jorge Matos <jisapjisap@gmail.com>
2024-11-16 16:04:36 +0100

    testimonial animations

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react";
+import React, { useState } from "react";
 import AshwinSantiago from "@/assets/images/ashwin-santiago.jpg";
 import AlecWhitten from "@/assets/images/alec-whitten.jpg";
 import ReneWells from "@/assets/images/rene-wells.jpg";
@@ -11,6 +11,7 @@ import test from "node:test";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { motion, AnimatePresence } from "framer-motion";
 
 export const testimonials = [
   {
@@ -43,48 +44,61 @@ export const testimonials = [
   },
 ];
 
-const SELECTED_TESTIMONIAL_INDEX = 0;
-
 export const Testimonials = () => {
+
+  const [testimonialIndex, setTestimonialIndex] = useState(0);
 
   return (
     <section id="testimonials">
       <div className="container">
         <SectionBorder borderTop>
           <SectionContent>
-            <div className="border-gradient rounded-3xl px-6 md:px-10 lg:px-16 py-16 lg:py-24 relative flex flex-col md:flex-row items-center gap-12 md:mx-10 lg:mx-20">
+            <div className="border-gradient rounded-3xl px-6 md:px-10 lg:px-16 py-16 lg:py-24 relative flex flex-col md:flex-row gap-12 md:mx-10 lg:mx-20">
               <FontAwesomeIcon 
                 icon={faQuoteLeft} 
                 className="absolute size-20 text-violet-400 top-0 left-6 md:left-10 lg:left-16 -translate-y-1/2"
               />
               {testimonials.map((testimonial, index) => (
-                <React.Fragment key={testimonial.name}>
-                  { SELECTED_TESTIMONIAL_INDEX  === index && (
-                    <blockquote 
-                      key={testimonial.name} 
-                      className="flex flex-col lg:flex-row gap-12"
-                    >
-                      <p className="text-xl md:text-2xl font-medium">
-                        {testimonial.quote}
-                      </p>
-                      <cite className="not-italic lg:text-right">
-                        <Image 
-                          src={testimonial.image} 
-                          alt={testimonial.name} 
-                          className="rounded-xl size-28 max-w-none" 
-                        />
-                        <div className="font-bold mt-4">{testimonial.name}</div>
-                        <div className="text-xs text-gray-400 mt-2">{testimonial.title}</div>
-                      </cite>
-                    </blockquote>
-                  )}                
-                </React.Fragment>
+                <AnimatePresence>
+                  <React.Fragment key={testimonial.name}>
+                      { testimonialIndex  === index && (
+                        <motion.blockquote 
+                          initial={{ opacity: 0, y: 25 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          exit={{ opacity: 0, y: 25 }}
+                          transition={{ duration: 0.5 }}
+                          key={testimonial.name} 
+                          className="flex flex-col lg:flex-row gap-12"
+                        >
+                          <p className="text-xl md:text-2xl font-medium">
+                            {testimonial.quote}
+                          </p>
+                          <cite className="not-italic lg:text-right">
+                            <Image 
+                              src={testimonial.image} 
+                              alt={testimonial.name} 
+                              className="rounded-xl size-28 max-w-none" 
+                            />
+                            <div className="font-bold mt-4">{testimonial.name}</div>
+                            <div className="text-xs text-gray-400 mt-2">{testimonial.title}</div>
+                          </cite>
+                        </motion.blockquote>
+                      )}                
+                  </React.Fragment>
+                </AnimatePresence>
               ))}
-              <div className="flex gap-2 md:flex-col justify-center">
+              <div className="flex gap-2 md:flex-col">
                 {testimonials.map((testimonial, index) => (
-                  <div key={testimonial.name} className="size-6 relative isolate inline-flex items-center justify-center">
-                    {SELECTED_TESTIMONIAL_INDEX === index && (
-                      <div className="absolute border-gradient size-full rounded-full -z-10"></div>
+                  <div 
+                    key={testimonial.name} 
+                    className="size-6 relative isolate inline-flex items-center justify-center"
+                    onClick={() => setTestimonialIndex(index)}
+                  >
+                    {testimonialIndex === index && (
+                      <motion.div 
+                        className="absolute border-gradient size-full rounded-full -z-10"
+                        layoutId="testimonial-dot"
+                      ></motion.div>
                     )}
                     <div className="size-1.5 bg-gray-200 rounded-full"></div>
                   </div>
